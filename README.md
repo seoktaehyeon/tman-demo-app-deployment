91b021a029f687315c0be31b7c523dfe4681f419
seoktaehyeon <seoktaehyeon@msn.com>
2022-06-21 12:12:20 +0800

    init repo

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Application Deployement Scripts Demo
+
+## Precondition for deployment
+
+- prepare yaml env file
+
+```bash
+opsctl env2yaml --input ${env_input} --output ${env_output}
+```
+
+- render template for deployment
+
+```bash
+opsctl tmpl2art --config ${env_output} --tmpl ${tmpl_path} --output .
+```
+
+## Append the deployment scripts
+
+append scripts into `deploy.sh`
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+env_input="env"
+env_output=".env"
+tmpl_path="template"
+[[ -f ${env_input} ]] || {
+    echo "===== ERROR ===== No env input file"
+    exit 1
+}
+[[ -d ${tmpl_path} ]] || {
+    echo "===== ERROR ===== Template path is required"
+    exit 1
+}
+opsctl env2yaml --input ${env_input} --output ${env_output}
+opsctl tmpl2art --config ${env_output} --tmpl ${tmpl_path} --output .
+#kubectl 
